f41688ea50aa710fe308eee325f6b4d985621e02
fauzanbakri <mfzn.zoldyck@gmail.com>
2025-03-04 13:41:00 +0800

    04032025

--- application/controllers/Hardcomplain.php
+++ application/controllers/Hardcomplain.php
@@ -33,11 +33,11 @@ class Hardcomplain extends CI_Controller {
 			@grup := tim
 		FROM 
 			tiket
-		WHERE prioritas='High'
 		LEFT JOIN 
 			olt ON olt.idOlt = tiket.idOlt
 		CROSS JOIN 
 			(SELECT @urutan := 0, @grup := '') AS vars
+		WHERE prioritas='High'
 		ORDER BY 
 			tim, prioritas, tanggal ASC;
 
