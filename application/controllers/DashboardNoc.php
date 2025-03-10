805d0166eeb08c6ea2da7b700d1c29cde76b31bb
fauzanbakri <mfzn.zoldyck@gmail.com>
2025-03-10 11:33:20 +0800

    10032025

--- application/controllers/DashboardNoc.php
+++ application/controllers/DashboardNoc.php
@@ -871,7 +871,7 @@ class DashboardNoc extends CI_Controller {
              GROUP BY tahun, minggu
          ) AS grouped_data
          GROUP BY minggu, tahun
-            ORDER BY minggu;
+            ORDER BY bulan;
      ");
      
      $result5 = $query->result_array();
