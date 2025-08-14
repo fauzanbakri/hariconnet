63162a60eaf71e987a2cab185ad2607c7f4e24d5
fauzanbakri <mfzn.zoldyck@gmail.com>
2025-08-14 11:04:07 +0800

    14082025

--- application/controllers/MonitoringSLA.php
+++ application/controllers/MonitoringSLA.php
@@ -21,7 +21,7 @@ class MonitoringSLA extends CI_Controller
         // if (!isset($_SESSION['role']) || !in_array($_SESSION['role'], array('Superadmin','NOC Ritel'))) {
         //     redirect('DashboardNoc'); return;
         // }
-
+        $this->load->view('navbar');
         $this->load->view('monitoringSLA');
     }
 
