edb98c1ac273ad175e481fffaba9b501227c7127
fauzanbakri <mfzn.zoldyck@gmail.com>
2025-08-21 10:54:22 +0800

    21082025

--- application/views/monitoringSLA.php
+++ application/views/monitoringSLA.php
@@ -36,33 +36,119 @@
                                 </div><!-- end card-body -->
                             </div><!-- end card -->
                         </div>
-                        <div class="col-xl-3 col-xl-6">
-                            <!-- card -->
-                            <div class="card card-animate bg-info">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center">
-                                        <div class="flex-grow-1">
-                                            <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
-                                        </div>
-                                        <div class="flex-shrink-0">
-                                            <h5 class="text-warning fs-14 mb-0">
-                                                <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
-                                            </h5>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex align-items-end justify-content-between mt-4">
-                                        <div>
-                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">36,894</span></h4>
-                                            <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
-                                        </div>
-                                        <div class="avatar-sm flex-shrink-0">
-                                            <span class="avatar-title bg-white bg-opacity-25 rounded fs-3">
-                                                <i class="bx bx-shopping-bag text-white"></i>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div><!-- end card body -->
-                            </div><!-- end card -->
+                        <div class="col-xl-6">
+                            <div class="row">
+                                <div class="col-xl-6">
+                                    <div class="card card-animate bg-info">
+                                        <div class="card-body">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-grow-1">
+                                                    <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
+                                                </div>
+                                                <div class="flex-shrink-0">
+                                                    <h5 class="text-warning fs-14 mb-0">
+                                                        <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                                <div>
+                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">36,894</span></h4>
+                                                    <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
+                                                </div>
+                                                <div class="avatar-sm flex-shrink-0">
+                                                    <span class="avatar-title bg-white bg-opacity-25 rounded fs-3">
+                                                        <i class="bx bx-shopping-bag text-white"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                </div>
+                                <div class="col-xl-6">
+                                    <div class="card card-animate bg-info">
+                                        <div class="card-body">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-grow-1">
+                                                    <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
+                                                </div>
+                                                <div class="flex-shrink-0">
+                                                    <h5 class="text-warning fs-14 mb-0">
+                                                        <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                                <div>
+                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">36,894</span></h4>
+                                                    <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
+                                                </div>
+                                                <div class="avatar-sm flex-shrink-0">
+                                                    <span class="avatar-title bg-white bg-opacity-25 rounded fs-3">
+                                                        <i class="bx bx-shopping-bag text-white"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xl-6">
+                                    <div class="card card-animate bg-info">
+                                        <div class="card-body">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-grow-1">
+                                                    <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
+                                                </div>
+                                                <div class="flex-shrink-0">
+                                                    <h5 class="text-warning fs-14 mb-0">
+                                                        <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                                <div>
+                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">36,894</span></h4>
+                                                    <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
+                                                </div>
+                                                <div class="avatar-sm flex-shrink-0">
+                                                    <span class="avatar-title bg-white bg-opacity-25 rounded fs-3">
+                                                        <i class="bx bx-shopping-bag text-white"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                </div>
+                                <div class="col-xl-6">
+                                    <div class="card card-animate bg-info">
+                                        <div class="card-body">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-grow-1">
+                                                    <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
+                                                </div>
+                                                <div class="flex-shrink-0">
+                                                    <h5 class="text-warning fs-14 mb-0">
+                                                        <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                                <div>
+                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">36,894</span></h4>
+                                                    <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
+                                                </div>
+                                                <div class="avatar-sm flex-shrink-0">
+                                                    <span class="avatar-title bg-white bg-opacity-25 rounded fs-3">
+                                                        <i class="bx bx-shopping-bag text-white"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                </div>
+                            </div>
                         </div>
                         <div class="col-xl-6">
                             <div class="card">
