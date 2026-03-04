a41d5b34a22f4e8c442cc5d0fbad2d96f3bbc3b5
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-04 15:14:06 +0800

    sql: update tim table schema (add telp, utf8mb4)

--- sql/create_table_tim.sql
+++ sql/create_table_tim.sql
@@ -1,9 +1,10 @@
 -- SQL to create `tim` table
--- Columns: idTim (int, PK, auto_increment), idBc (varchar(10)), nama (varchar(100)), chatId (varchar(100))
+-- Schema based on provided screenshot
 CREATE TABLE IF NOT EXISTS `tim` (
   `idTim` int(10) NOT NULL AUTO_INCREMENT,
   `idBc` varchar(10) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `chatId` varchar(100) NOT NULL,
+  `telp` varchar(15) NOT NULL,
   PRIMARY KEY (`idTim`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
