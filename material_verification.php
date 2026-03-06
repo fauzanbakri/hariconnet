1573c4177be1c87dee1dac90cce4b3efcf701b14
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-06 10:04:09 +0800

    Add vendor dependencies including PhpSpreadsheet

---
+++ material_verification.php
@@ -0,0 +1,5 @@
+<?php
+// Simple verification page to confirm which commit the server is serving
+// Open /sibt/material_verification.php in your browser
+echo "MATERIAL VIEW UPDATED - commit: 4da5358f4afd4de66f73d6926d4387c0522528cf";
+?>
