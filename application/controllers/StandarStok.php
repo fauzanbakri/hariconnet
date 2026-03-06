185210921fdd4eceb728a7c4a7df3ac6f807c9ca
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-06 15:37:58 +0800

    Add vendor dependencies including PhpSpreadsheet

--- application/controllers/StandarStok.php
+++ application/controllers/StandarStok.php
@@ -14,13 +14,58 @@ class StandarStok extends CI_Controller {
         $title['title'] = "Standar Stok";
 		session_start();
         // Fetch rows from standarStok table (assumes table exists)
-        $data['rows'] = [];
-        if ($this->db->table_exists('standarStok')) {
-            $this->db->order_by('idStandarStok', 'DESC');
-            $data['rows'] = $this->db->get('standarStok')->result();
-        }
+            $data['rows'] = [];
+            if ($this->db->table_exists('standarStok')) {
+                $this->db->order_by('idStandarStok', 'DESC');
+                $data['rows'] = $this->db->get('standarStok')->result();
+            }
+
+            // fetch basecamp list for dropdown (sloc)
+            $data['basecamp'] = [];
+            if ($this->db->table_exists('basecamp')) {
+                $this->db->select('idBc, sloc');
+                $this->db->from('basecamp');
+                $this->db->order_by('sloc', 'ASC');
+                $data['basecamp'] = $this->db->get()->result();
+            }
 
         $this->load->view('navbar', $title);
         $this->load->view('standar_stok', $data);
     }
 }
+    
+    public function insertData()
+    {
+        // minimal server-side validation
+        $idBc = $this->input->post('idBc');
+        if (!$idBc) {
+            echo 'Basecamp wajib dipilih';
+            return;
+        }
+
+        $payload = [
+            'idBc' => $idBc,
+            'ont_huawei' => $this->input->post('ont_huawei') ?: 0,
+            'ont_fiberhome' => $this->input->post('ont_fiberhome') ?: 0,
+            'ont_zte' => $this->input->post('ont_zte') ?: 0,
+            'ont_raisecom' => $this->input->post('ont_raisecom') ?: 0,
+            'ont_bdcom' => $this->input->post('ont_bdcom') ?: 0,
+            'dw_50' => $this->input->post('dw_50') ?: 0,
+            'dw_100' => $this->input->post('dw_100') ?: 0,
+            'dw_150' => $this->input->post('dw_150') ?: 0,
+            'dw_250' => $this->input->post('dw_250') ?: 0,
+            'dw_300' => $this->input->post('dw_300') ?: 0,
+            'dw_1000' => $this->input->post('dw_1000') ?: 0,
+            'adss_6c' => $this->input->post('adss_6c') ?: 0,
+            'adss_24c' => $this->input->post('adss_24c') ?: 0,
+            'adss_48c' => $this->input->post('adss_48c') ?: 0,
+            'adss_96c' => $this->input->post('adss_96c') ?: 0
+        ];
+
+        if ($this->db->insert('standarStok', $payload)) {
+            echo 'success';
+        } else {
+            $e = $this->db->error();
+            echo isset($e['message']) ? $e['message'] : 'Gagal menyimpan';
+        }
+    }
--- application/views/standar_stok.php
+++ application/views/standar_stok.php
@@ -22,6 +22,9 @@
                             <div class="card">
                                 <div class="card-header">
                                     <h5 class="card-title mb-0">Tabel Standar Stok</h5>
+                                    <div class="float-end">
+                                        <button class="btn btn-primary btn-sm" id="btnAddStandar">Input Standar Stok</button>
+                                    </div>
                                 </div>
                                 <div class="card-body">
                                     <table id="standarStokTable" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
@@ -83,6 +86,95 @@
                         </div>
                     </div>
 
+                        <!-- Modal Input Standar Stok -->
+                        <div class="modal fade" id="modalInputStandar" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Input Standar Stok</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="formInputStandar">
+                                            <div class="row g-2">
+                                                <div class="col-md-6">
+                                                    <label class="form-label">Basecamp (SLOC)</label>
+                                                    <select id="inputIdBc" class="form-select">
+                                                        <option value="">Pilih Basecamp</option>
+                                                        <?php foreach (($basecamp ?? []) as $bc) { echo '<option value="'.$bc->idBc.'">'.htmlspecialchars($bc->sloc).'</option>'; } ?>
+                                                    </select>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ONT HUAWEI</label>
+                                                    <input type="number" id="ont_huawei" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ONT FIBERHOME</label>
+                                                    <input type="number" id="ont_fiberhome" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ONT ZTE</label>
+                                                    <input type="number" id="ont_zte" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ONT RAISECOM</label>
+                                                    <input type="number" id="ont_raisecom" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ONT BDCOM</label>
+                                                    <input type="number" id="ont_bdcom" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 50</label>
+                                                    <input type="number" id="dw_50" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 100</label>
+                                                    <input type="number" id="dw_100" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 150</label>
+                                                    <input type="number" id="dw_150" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 250</label>
+                                                    <input type="number" id="dw_250" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 300</label>
+                                                    <input type="number" id="dw_300" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">DW 1000</label>
+                                                    <input type="number" id="dw_1000" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ADSS 6C</label>
+                                                    <input type="number" id="adss_6c" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ADSS 24C</label>
+                                                    <input type="number" id="adss_24c" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ADSS 48C</label>
+                                                    <input type="number" id="adss_48c" class="form-control" value="0">
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <label class="form-label">ADSS 96C</label>
+                                                    <input type="number" id="adss_96c" class="form-control" value="0">
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                        <button type="button" class="btn btn-primary" id="saveStandarBtn">Simpan</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                 </div>
             </div>
 
@@ -108,6 +200,54 @@
             lengthMenu: [[-1,10,25,50], ['All',10,25,50]],
             responsive: true,
             order: []
+        });
+    });
+</script>
+<script>
+    $(function(){
+        $('#btnAddStandar').on('click', function(){
+            var modal = new bootstrap.Modal(document.getElementById('modalInputStandar'));
+            modal.show();
+        });
+
+        $('#saveStandarBtn').on('click', function(){
+            var payload = {
+                idBc: $('#inputIdBc').val(),
+                ont_huawei: $('#ont_huawei').val(),
+                ont_fiberhome: $('#ont_fiberhome').val(),
+                ont_zte: $('#ont_zte').val(),
+                ont_raisecom: $('#ont_raisecom').val(),
+                ont_bdcom: $('#ont_bdcom').val(),
+                dw_50: $('#dw_50').val(),
+                dw_100: $('#dw_100').val(),
+                dw_150: $('#dw_150').val(),
+                dw_250: $('#dw_250').val(),
+                dw_300: $('#dw_300').val(),
+                dw_1000: $('#dw_1000').val(),
+                adss_6c: $('#adss_6c').val(),
+                adss_24c: $('#adss_24c').val(),
+                adss_48c: $('#adss_48c').val(),
+                adss_96c: $('#adss_96c').val()
+            };
+
+            if (!payload.idBc) {
+                alert('Pilih Basecamp (SLOC) terlebih dahulu');
+                return;
+            }
+
+            $.ajax({
+                url: 'StandarStok/insertData',
+                type: 'POST',
+                data: payload,
+                success: function(resp){
+                    if (resp === 'success') {
+                        location.reload();
+                    } else {
+                        alert('Gagal: ' + resp);
+                    }
+                },
+                error: function(xhr){ alert('Error: ' + xhr.responseText); }
+            });
         });
     });
 </script>
