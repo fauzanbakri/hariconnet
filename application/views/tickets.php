585c4987ae58c1aebfb416b46bd849b7628e364c
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-11 12:46:37 +0800

    Add vendor dependencies including PhpSpreadsheet

--- application/views/tickets.php
+++ application/views/tickets.php
@@ -536,30 +536,9 @@
     })();
     </script>
 
-    <script src="js/code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <!--datatable js-->
-    <script src="js/cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="js/cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-    <script src="js/cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-    <script src="js/cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
-    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
-    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
-    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="js/cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    
-    <script src="assets/libs/sweetalert2/sweetalert2.min.js"></script>
-    <!-- <script src="assets/js/pages/sweetalerts.init.js"></script> -->
-
-    <!-- multi.js -->
+    <!-- page-specific libs -->
     <script src="assets/libs/multi.js/multi.min.js"></script>
-    <!-- autocomplete js -->
     <script src="assets/libs/%40tarekraafat/autocomplete.js/autoComplete.min.js"></script>
-    <script src="assets/js/pages/datatables.init.js"></script>
-    <script src="assets/js/plugins.js"></script>
-
-    <script src="assets/js/app.js"></script>   
 
 
     <script>
