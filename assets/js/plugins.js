9c9f2622778b11dee21ec8d0bd4027e7d0e62847
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-11 10:46:10 +0800

    Add vendor dependencies including PhpSpreadsheet

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,1 +1,18 @@
-(document.querySelectorAll("[toast-list]")||document.querySelectorAll("[data-choices]")||document.querySelectorAll("[data-provider]"))&&(document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/choices.js/public/assets/scripts/choices.min.js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/flatpickr/flatpickr.min.js'><\/script>"));
+(function(){
+	var need = document.querySelector("[toast-list]") || document.querySelector("[data-choices]") || document.querySelector("[data-provider]");
+	if (!need) return;
+	var scripts = [
+		'https://cdn.jsdelivr.net/npm/toastify-js',
+		'assets/libs/choices.js/public/assets/scripts/choices.min.js',
+		'assets/libs/flatpickr/flatpickr.min.js'
+	];
+	scripts.forEach(function(src){
+		try{
+			var s = document.createElement('script');
+			s.type = 'text/javascript';
+			s.src = src;
+			s.async = false;
+			document.head.appendChild(s);
+		}catch(e){}
+	});
+})();
