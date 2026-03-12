47f887d82363d044744de66fff306caec0d2c91b
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-12 10:09:12 +0800

    Add vendor dependencies including PhpSpreadsheet

--- application/views/tickets.php
+++ application/views/tickets.php
@@ -514,7 +514,16 @@
     <script src="assets/libs/feather-icons/feather.min.js"></script>
     <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
     <!-- <script src="assets/js/plugins.js"></script> -->
-
+<!--datatable js-->
+    <script src="js/cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="js/cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="js/cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script>
     (function(){
         // Ensure any leftover modal backdrops are removed when modals are hidden
