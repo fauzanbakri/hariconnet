9f8061823a8769c36d38927f6e6093fb92945cce
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-13 08:56:49 +0800

    new comm

--- application/views/tickets.php
+++ application/views/tickets.php
@@ -524,6 +524,11 @@
     <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="js/cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+
+        <!-- prismjs plugin -->
+    <script src="assets/libs/prismjs/prism.js"></script>
+    <script src="js/code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/libs/sweetalert2/sweetalert2.min.js"></script>
     <script>
     (function(){
         // Ensure any leftover modal backdrops are removed when modals are hidden
