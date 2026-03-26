396f1b29d22dc68afd6d50bb8a3a6de9dc9d751a
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-03-26 10:11:27 +0800

    new comm

--- application/views/monitoring_material.php
+++ application/views/monitoring_material.php
@@ -132,7 +132,40 @@
 </div>
 
 <!-- No DataTable needed for card view -->
+  <!-- JAVASCRIPT -->
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/node-waves/waves.min.js"></script>
+    <script src="assets/libs/feather-icons/feather.min.js"></script>
+    <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <!-- <script src="assets/js/plugins.js"></script> -->
 
+    <script src="js/code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <!--datatable js-->
+    <script src="js/cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="js/cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="js/cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+    <script src="js/cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="js/cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="js/cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+    
+    <script src="assets/libs/sweetalert2/sweetalert2.min.js"></script>
+    <!-- <script src="assets/js/pages/sweetalerts.init.js"></script> -->
+
+    <!-- multi.js -->
+    <script src="assets/libs/multi.js/multi.min.js"></script>
+    <!-- autocomplete js -->
+    <script src="assets/libs/%40tarekraafat/autocomplete.js/autoComplete.min.js"></script>
+    <script src="assets/js/pages/datatables.init.js"></script>
+    <script src="assets/js/plugins.js"></script>
+
+    <script src="assets/js/app.js"></script>   
 <?php if (isset($_GET['debug']) && $_GET['debug']=='1') { ?>
 <script>
     try {
