f5837da1e6a2690cc572a883dcd9e4ea247a1b4f
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-06-19 21:27:33 +0800

    new

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -668,7 +668,7 @@
                                                 <a href="Report" class="nav-link" data-key="t-analytics"> Report Shift </a>
                                             </li>
                                             <li class="nav-item">
-                                                <a href="ShiftCorpo/showReport" class="nav-link" data-key="t-analytics"> Report Shift Corpo </a>
+                                                <a href="ShiftCorpo" class="nav-link" data-key="t-analytics"> Report Shift Corpo </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a href="ListPending" class="nav-link" data-key="t-crm"> List Pending Team </a>
