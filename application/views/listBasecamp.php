775631c718dda284ecaf0b677d7511c267156719
fauzanbakri <mfzn.zoldyck@gmail.com>
2026-06-24 09:35:41 +0800

    new

--- application/views/listBasecamp.php
+++ application/views/listBasecamp.php
@@ -45,7 +45,7 @@
                                                                     <option value="KENDARI">KENDARI</option>
                                                                     <option value="MANADO">MANADO</option>
                                                                     <option value="AMBON">AMBON</option>
-                                                                    <option value="PAPUA">PAPUA & PAPUA BARAT</option>
+                                                                    <option value="PAPUA & PAPUA BARAT">PAPUA & PAPUA BARAT</option>
                                                                 </select>
                                                             </div>
                                                         </div>
@@ -145,7 +145,7 @@
                                                                     <option value="KENDARI">KENDARI</option>
                                                                     <option value="MANADO">MANADO</option>
                                                                     <option value="AMBON">AMBON</option>
-                                                                    <option value="PAPUA">PAPUA & PAPUA BARAT</option>
+                                                                    <option value="PAPUA & PAPUA BARAT">PAPUA & PAPUA BARAT</option>
                                                                 </select>
                                                             </div>
                                                         </div>
